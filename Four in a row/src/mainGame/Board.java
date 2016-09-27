e35d4c675fa8fd99b2343be07836fcd68723c682
JOCompute <joel.w.ottosson@gmail.com>
2016-09-27 12:54:23 +0200

    Börjat återsätall alt

--- Four in a row/src/mainGame/Board.java
+++ Four in a row/src/mainGame/Board.java
@@ -5,7 +5,9 @@ import javax.swing.*;
 import java.awt.Dimension;
 
 public class Board {
-
+	public static final int COLFULL = -1;
+	public static final int COLNOTFULL = 0;
+	
 	private int [][] board;
 	private int toWin;
 	private Dimension cell;
@@ -30,6 +32,7 @@ public class Board {
 		
 	}
 	
+	
 	/**
 	 * Set all cells to EMPTY, = 0;
 	 */
@@ -41,6 +44,82 @@ public class Board {
 		}
 	}
 	
+	public int firstEmptyRow(int col){
+		for(int tempRow = board.length; tempRow >= 0; tempRow--){
+			if(this.board[tempRow][col] == Piece.EMPTY){
+				return tempRow;
+			}
+		}
+		
+		return COLFULL;
+	}
+	
+	public void setPiece(int row, int col, int turn){
+		this.board[row][col] = turn;
+	}
+	
+	
+	public boolean checkForWin(int row, int col, int turn){
+		
+		// Torn
+		int counter=0;
+		for(int temp=-this.toWin;temp<=toWin; temp++){				
+				try{
+					counter = this.board[row+temp][col] == turn ? counter+1:0;
+				}catch (ArrayIndexOutOfBoundsException e){
+					
+				}		
+				
+				if(counter == toWin){
+					return true;
+				}
+			}
+		
+		//Row
+		counter=0;
+		for(int temp=-this.toWin;temp<=toWin; temp++){				
+			try{
+				counter = this.board[row][col+temp] == turn ? counter+1:0;
+			}catch (ArrayIndexOutOfBoundsException e){
+				
+			}		
+			
+			if(counter == toWin){
+				return true;
+			}
+		}
+		
+		
+		//HU -> NV
+		counter=0;
+		for(int temp=-this.toWin;temp<=toWin; temp++){				
+			try{
+				counter = this.board[row+temp][col+temp] == turn ? counter+1:0;
+			}catch (ArrayIndexOutOfBoundsException e){
+				
+			}		
+			
+			if(counter == toWin){
+				return true;
+			}
+		}
+	
+		//HN -> VU
+		counter=0;
+		for(int temp=-this.toWin;temp<=toWin; temp++){				
+			try{
+				counter = this.board[row+temp][col-temp] == turn ? counter+1:0;
+			}catch (ArrayIndexOutOfBoundsException e){
+				
+			}		
+			
+			if(counter == toWin){
+				return true;
+			}
+		}
+	
+		return false;
+	}
 	
 	
 }
--- Four in a row/src/mainGame/GamePlay.java
+++ Four in a row/src/mainGame/GamePlay.java
@@ -1,22 +1,62 @@
 package mainGame;
 
 import java.awt.Dimension;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class GamePlay {
-
-	int numRow = 6;
-	int numCol = 7;
-	int toWin =4;
-	Dimension cell = new Dimension(40,40);
 	
 	public static void main(String[] args) {
 		int numRow = 6;
 		int numCol = 7;
 		int toWin =4;
 		Dimension cell = new Dimension(40,40);
+		int turn = Piece.EMPTY;
+		boolean gameON = true;
+		boolean rightInput= true;
+		
 		
 		Board myGame = new Board(numRow, numCol, toWin, cell);
-
+		Scanner input = new Scanner(System.in);
+		
+		int placeRow=-1;
+		int placeCol=-1;
+		
+		do{
+			switch(turn){
+				case Piece.GREEN :
+					turn = Piece.RED;
+					break;
+				case Piece.RED:
+					turn = Piece.GREEN;
+					break;
+				case Piece.EMPTY:
+					turn = Piece.GREEN;
+					break;
+			}
+			
+			do{
+				try{
+					System.out.print("Please enter the column nr you want to play in: ");
+					placeCol = input.nextInt();
+					placeRow = myGame.firstEmptyRow(placeCol);
+					if(placeRow == Board.COLFULL) throw new ColumnIsFullException();
+					myGame.setPiece(placeRow, placeCol, turn);
+					rightInput=true;
+				}catch (InputMismatchException e){
+					
+					rightInput=false;
+				}catch (ArrayIndexOutOfBoundsException e){
+					rightInput=false;
+				}catch (ColumnIsFullException e){
+					rightInput=false;
+				}
+			}while(! rightInput);
+			
+			gameON = myGame.checkForWin(placeRow, placeCol, turn) ? false : true;
+			
+		}while(gameON);
+		System.out.println("Det äe slut");
 	}
 
 }
